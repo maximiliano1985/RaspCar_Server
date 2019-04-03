bad7cbdd92e7568540a5731bcf3c0b641883423d
maximiliano1985 <maximiliano_giorgio@yahoo.it>
2019-04-03 20:26:07 +0000

    Updated scripts to work on chip user

--- etc/prepare_os.sh
+++ etc/prepare_os.sh
@@ -11,9 +11,9 @@ sudo pip3 install datetime
 sudo pip3 install obd
 
 echo "### Creating folder for logs (available only for pi user)"
-sudo mkdir /home/pi/Documents/
-sudo mkdir /home/pi/Documents/logs
-sudo mkdir /home/pi/Documents/logs/usb_share
-sudo mkdir /home/pi/Documents/logs/obd_logs
-sudo mkdir /home/pi/Documents/logs/obddata_logs
-sudo chmod -R 777 /home/pi/Documents/logs
+sudo mkdir /home/chip/Documents/
+sudo mkdir /home/chip/Documents/logs
+sudo mkdir /home/chip/Documents/logs/usb_share
+sudo mkdir /home/chip/Documents/logs/obd_logs
+sudo mkdir /home/chip/Documents/logs/obddata_logs
+sudo chmod -R 777 /home/chip/Documents/logs
