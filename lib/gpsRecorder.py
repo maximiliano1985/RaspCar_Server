21a82dfa92be2c4ce100a74b372795b7ab61f2fa
Maximiliano Giorgio <maximiliano_giorgio@yahoo.it>
2019-09-26 20:39:50 +0100

    Correcting code

--- lib/gpsRecorder.py
+++ lib/gpsRecorder.py
@@ -15,7 +15,7 @@ class gpsRecorder(object):
     def __init__(self, port='/dev/ttyS0', baudrate = 19200, debug = False):
        self.debug = debug
        
-       self.header = "time_gps;status_gps;latitude_gps;NS_indic_gps;longitude_gps;EW_indic_gps;speed_gps;course_gps;utc_gps;mode_gps;"
+       #self.header = "time_gps;status_gps;latitude_gps;NS_indic_gps;longitude_gps;EW_indic_gps;speed_gps;course_gps;utc_gps;mode_gps;"
        
        self.ser = serial.Serial(
            port=port,
@@ -29,7 +29,7 @@ class gpsRecorder(object):
         line = ser.readline()
         # $GPRMC,hhmmss.sss,A,dddmm.mmmm,a,dddmm.mmmm,a,x.x,x.x,ddmmyy,,,a*hh<CR><LF>
         #   time: in hhmmss.sss
-        #   status: ‘V’ = Navigation receiver warning, ‘A’ = Data Valid
+        #   status: V = Navigation receiver warning, A = Data Valid
         #   latitude: dddmm.mmmm 
         #   NS_indic: N north, S south
         #   longitude: dddmm.mmmm 
@@ -37,12 +37,12 @@ class gpsRecorder(object):
         #   speed: Speed over ground in knots
         #   course: Course over ground in degrees
         #   utc: UTC date of position fix, ddmmyy format
-        #   mode: Mode indicator ‘N’ = Data not valid,
-        #                        ‘A’ = Autonomous mode,
-        #                        ‘D’ = Differential mode,
-        #                        ‘E’ = Estimated (dead reckoning) mode,
-        #                        ‘M’ = Manual input mode,
-        #                        ‘S’ = Simulator mode
+        #   mode: Mode indicator N = Data not valid,
+        #                        A = Autonomous mode,
+        #                        D = Differential mode,
+        #                        E = Estimated (dead reckoning) mode,
+        #                        M = Manual input mode,
+        #                        S = Simulator mode
         if self.debug:
             print(line)
         return [time, status, latitude, NS_indic, longitude, EW_indic, speed, course, utc, mode]
