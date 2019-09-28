0929e789e2c9a78f22194992917b54d74b740c0e
Maximiliano Giorgio <maximiliano_giorgio@yahoo.it>
2019-09-28 10:21:49 +0100

    corrected services definition

--- etc/install_services.sh
+++ etc/install_services.sh
@@ -1,24 +1,24 @@
 echo "### INSTALLING SERVICES ###"
 
 echo "### Installation of service for automatic USB management"
-sudo cp ../lib/smartMassUSB.py      /usr/local/share
-sudo cp ../etc/smartMassUSB.service /etc/systemd/system
+sudo cp ../lib/smartmassusb.py      /usr/local/share
+sudo cp ../etc/smartmassusb.service /etc/systemd/system
 
 echo "### Installation of service for battery management"
-sudo cp ../lib/powerManagement.py /usr/local/share
-sudo cp ../etc/powerManagement.service /etc/systemd/system
+sudo cp ../lib/powermanagement.py /usr/local/share
+sudo cp ../etc/powermanagement.service /etc/systemd/system
 
 echo "### Installation of service for automatic data recording"
-sudo cp ../lib/dataRecorder.py 	  /usr/local/share
-sudo cp ../etc/dataRecorder.service /etc/systemd/system
+sudo cp ../lib/datarecorder.py 	  /usr/local/share
+sudo cp ../etc/datarecorder.service /etc/systemd/system
 
 echo "### Installation of service for automatic obd connecting"
 sudo cp ../lib/obd_connect.py     /usr/local/share
 sudo cp ../etc/obdconnect.service /etc/systemd/system
 
 echo "### Installation of service for log file monitoring"
-sudo cp logManager.py      		  /usr/local/share
-sudo cp ../etc/logManager.service /etc/systemd/system
+sudo cp logmanager.py      		  /usr/local/share
+sudo cp ../etc/logmanager.service /etc/systemd/system
 
 echo "### Installation of libraries"
 sudo cp ../lib/fileLogger.py 	  /usr/local/share
@@ -27,29 +27,29 @@ sudo cp ../lib/gpsRecorder.py     /usr/local/share
 
 echo "### Enabling installed services"
 cd /usr/local/share/
-sudo chmod +x smartMassUSB.py
-sudo chmod +x powerManagement.py
-sudo chmod +x dataRecorder.py
+sudo chmod +x smartmassusb.py
+sudo chmod +x powermanagement.py
+sudo chmod +x datarecorder.py
 sudo chmod +x obd_connect.py
-sudo chmod +x logManager.py
+sudo chmod +x logmanager.py
 
 cd /etc/systemd/system
 sudo systemctl daemon-reload
 
-sudo systemctl enable smartMassUSB.service
-sudo systemctl start  smartMassUSB.service
+sudo systemctl enable smartmassusb.service
+sudo systemctl start  smartmassusb.service
 
-sudo systemctl enable powerManagement.service
-sudo systemctl start  powerManagement.service
+sudo systemctl enable powermanagement.service
+sudo systemctl start  powermanagement.service
 
-sudo systemctl enable dataRecorder.service
-sudo systemctl start  dataRecorder.service
+sudo systemctl enable datarecorder.service
+sudo systemctl start  datarecorder.service
 
 sudo systemctl enable obdconnect.service
 sudo systemctl start  obdconnect.service
 
-sudo systemctl enable logManager.service
-sudo systemctl start  logManager.service
+sudo systemctl enable logmanager.service
+sudo systemctl start  logmanager.service
 
 
 #######
